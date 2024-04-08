24517f99eb4e820cfba9efd3d9e1bb97db9286b0
voidboe <127174664+iShan61322@users.noreply.github.com>
2024-04-08 18:05:25 +0800

    Add files via upload

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+#define Day 7
+int main()
+{
+	cout << "Hello world" << endl;
+	
+	int a = 1;
+	cout << "a= " << a<<endl;
+
+	cout << "A week is " << Day << "days" << endl;
+
+	const int month = 12;
+	cout << "A year is " << month << "months" << endl;
+	system("pause");
+	return 0;
+}
